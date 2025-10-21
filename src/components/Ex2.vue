1036feabcbc37ba48285214d7bef3279d3536461
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-21 23:45:03 +0800

    finished ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -6,6 +6,14 @@
                 items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
             }
             
+        },
+        methods: {
+            deleteItem (index) {
+                this.items.splice(index, 1)
+            },
+            addItem (item) {
+                this.items.push(item)
+            }
         }
     }
 </script>
@@ -14,5 +22,19 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul>
+        <li v-for="(item, index) in items" >
+            {{ item }} <button @click="deleteItem(index)">Delete!</button>
+        </li>
+     </ul>
+
+     <!-- this part is for the text input and the button -->
+    <input type="text" v-model="newItem">
+
+    </input>
+
+    <button @click="addItem(newItem)">
+        Add!
+    </button>
    
 </template>
